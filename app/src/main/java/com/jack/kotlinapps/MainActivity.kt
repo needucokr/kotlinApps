4d5b307bba4926513783bbb6ed7f444970eb8315
needucokr <49503436+needucokr@users.noreply.github.com>
2021-02-20 11:39:41 +0900

    test

--- app/src/main/java/com/jack/kotlinapps/MainActivity.kt
+++ app/src/main/java/com/jack/kotlinapps/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jack.kotlinapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//testsss
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
